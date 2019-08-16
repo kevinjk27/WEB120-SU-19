7f7b1e774c7345a780410e8b88f69de46cafd7f1
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2019-08-15 23:24:37 -0700

    Add files via upload

---
+++ big/calendar.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php'?>
+
+
+
+<div class='embed-container month-view'>
+    <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=ZmFzaTUwbGcxMmFqOHRtY2ljbWx1MTU4dmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161&showTitle=0' style='border-width:0' width='800' height='600' frameborder='0' scrolling='no'></iframe></div>
+
+<div class='embed-container agenda-view'>
+    <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=ZmFzaTUwbGcxMmFqOHRtY2ljbWx1MTU4dmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161&mode=AGENDA&showTitle=0' style='border-width:0' width='800' height='600' frameborder='0' scrolling='no'></iframe></div>
+
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+<h3>Get Your Tickets here!</h3>
+    <a href='https://www.ticketmaster.com/seattle-seahawks-tickets/artist/806020' target="_blank"><img src="images/seahawks.png" alt="seahwaks-logo"> </a>
+<br />
+<a href='https://www.vividseats.com/mlb-baseball/seattle-mariners-tickets/mariners-vs-blue-jays-8-25-2821884.html' target="_blank"><img src="images/mariners.png" alt="mariners-logo"></a>
+ 
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include 'includes/footer.php'?>
+ 
